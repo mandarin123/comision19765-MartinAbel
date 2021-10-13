d65546e58526d3a8b3be1ae6fc01b4e390f7eb6a
Martin Abel <martin.g.abel.d@gmail.com>
2021-10-13 11:56:22 -0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Prsos re cachetuda
+          Proyecto E Commerce comision 19765 Martin Abel
         </p>
         <a
           className="App-link"
