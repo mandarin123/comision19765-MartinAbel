933b624b270c721a31f5a23c6cbdef84d305a70f
Martin Abel <martin.g.abel.d@gmail.com>
2021-10-18 13:50:39 -0300

    Añadido Navbar

---
+++ src/components/NavBar.jsx
@@ -0,0 +1,115 @@
+import { Nav,
+    NavItem,
+    Dropdown,
+    DropdownItem,
+    DropdownToggle,
+    DropdownMenu,
+    NavLink,
+    Button,
+    Modal,
+    ModalHeader,
+    ModalBody,
+    ModalFooter
+} from 'reactstrap';
+import React, { useState } from 'react';
+
+const NavBar = (props) => {
+
+    const [dropdownOpen, setDropdownOpen] = useState(false);
+
+    const toggle = () => setDropdownOpen(!dropdownOpen);
+
+    const [modal, setModal] = useState(false);
+
+    const toggleModal = () => setModal(!modal);
+
+    const [modal2, setModal2] = useState(false);
+
+    const toggleModal2 = () => setModal2(!modal2);
+    
+
+    return (
+      <div>
+        <Nav pills>
+          <NavItem>
+            <NavLink href="#">Home</NavLink>
+          </NavItem>
+
+          <Dropdown nav isOpen={dropdownOpen} toggle={toggle}>
+            <DropdownToggle nav caret>
+              Productos
+            </DropdownToggle>
+            <DropdownMenu>
+              <DropdownItem>Tortas</DropdownItem>
+              <DropdownItem>Postres</DropdownItem>
+              <DropdownItem>Pasteleria</DropdownItem>
+              <DropdownItem divider />
+              <DropdownItem>Eventos Especiales</DropdownItem>
+            </DropdownMenu>
+          </Dropdown>
+
+          <NavItem>
+            <div>
+              <Button color="link" onClick={toggleModal2} style={{ textDecoration: "none" }}>
+                Sobre Nostros
+              </Button>
+              <Modal isOpen={modal2} toggle={toggleModal2}>
+                <ModalHeader toggle={toggleModal2}>Las Alba</ModalHeader>
+                <ModalBody>
+                  Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed
+                  do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+                  Ut enim ad minim veniam, quis nostrud exercitation ullamco
+                  laboris nisi ut aliquip ex ea commodo consequat. Duis aute
+                  irure dolor in reprehenderit in voluptate velit esse cillum
+                  dolore eu fugiat nulla pariatur. Excepteur sint occaecat
+                  cupidatat non proident, sunt in culpa qui officia deserunt
+                  mollit anim id est laborum.
+                </ModalBody>
+                <ModalFooter>
+                  <Button color="secondary">
+                    Daniela
+                  </Button>
+                  <Button color="secondary">
+                    Susana
+                  </Button>
+                </ModalFooter>
+              </Modal>
+            </div>
+          </NavItem>
+
+          <NavItem>
+            <div>
+              <Button color="link" onClick={toggleModal} style={{ textDecoration: "none" }}>
+                Contacto
+              </Button>
+              <Modal isOpen={modal} toggle={toggleModal}>
+                <ModalHeader toggle={toggleModal}>Para comunicarte con nosotros</ModalHeader>
+                <ModalBody>
+                  Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed
+                  do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+                  Ut enim ad minim veniam, quis nostrud exercitation ullamco
+                  laboris nisi ut aliquip ex ea commodo consequat. Duis aute
+                  irure dolor in reprehenderit in voluptate velit esse cillum
+                  dolore eu fugiat nulla pariatur. Excepteur sint occaecat
+                  cupidatat non proident, sunt in culpa qui officia deserunt
+                  mollit anim id est laborum.
+                </ModalBody>
+                <ModalFooter>
+                  <Button color="secondary">
+                    Whatsapp
+                  </Button>
+                  <Button color="secondary">
+                    Instragram
+                  </Button>
+                </ModalFooter>
+              </Modal>
+            </div>
+          </NavItem>
+
+        </Nav>
+      </div>
+    );
+};
+
+export default NavBar;
+
